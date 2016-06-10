bf8cfd187fea950f97d1238edd823e93153b48a9
Leslie Huang <lesliehuang@gmail.com>
2016-06-10 15:03:13 -0400

    updated

--- sentiment analysis.R
+++ sentiment analysis.R
@@ -2,11 +2,198 @@
 # LIWC analysis of FARC communiques
 
 rm(list=ls())
-setwd("/Users/lesliehuang/Dropbox/MA/")
+setwd("/Users/lesliehuang/Dropbox/MA-thesis-analysis/")
 
-libraries <- c("foreign", "utils", "stargazer", "dplyr", "devtools", "quanteda", "quantedaData", "ggplot2", "stringr")
+libraries <- c("foreign", "utils", "stargazer", "dplyr", "devtools", "quanteda", "quantedaData", "ggplot2", "stringr", "LIWCalike", "topicmodels", "lda", "stm", "LDAvis", "austin")
 lapply(libraries, require, character.only=TRUE)
 
+# get LIWC dict
 spanish_dict <- dictionary(file = "../LIWC/Spanish_LIWC2007_Dictionary.dic", format = "LIWC")
 
+# some major dates for plotting
+major_violence <- as.Date(c("7/20/13", "1/16/13", "7/29/14", "11/16/14", "4/15/15", "5/31/15", "6/15/15", "6/22/15"), "%m/%d/%y")
+major_agree <- as.Date(c("8/26/12", "5/26/13", "11/6/13", "5/16/14", "3/7/15", "6/2/15", "9/23/15"), "%m/%d/%y")
+# cf_start <- as.Date(c("11/20/12", "12/15/13", "5/16/14", "12/20/14", "7/20/15"), "%m/%d/%y")
+# cf_end <- as.Date(c("1/20/13", "1/15/14", "5/28/14", "5/22/15", "1/1/16"), "%m/%d/%y")
+
+ceasefires <- data.frame(start = as.Date(c("11/20/12", "12/15/13", "5/16/14", "12/20/14", "7/20/15"), "%m/%d/%y"), end = as.Date(c("1/20/13", "1/15/14", "5/28/14", "5/22/15", "1/1/16"), "%m/%d/%y"))
+
+# dataframe of all dates
+dates <- rbind(data.frame(date = major_violence, group = "major_viol"), data.frame(date = major_agree, group = "major_agree"))
+
+####################################################################################
 # import FARC communiques
+FARC <- read.csv("../MA-datasets/FARC_communiques.csv", stringsAsFactors = FALSE)
+
+# metadata: get date
+FARC_meta <- select(FARC, date)
+FARC_dates <- as.Date(FARC_meta[[1]], "%Y-%m-%d")
+
+# create corpus
+FARC_corp <- corpus(FARC$text, docvars = FARC_meta)
+
+FARC_dfm <- dfm(FARC_corp, language = "spanish", stem = TRUE, ignoredFeatures = stopwords("spanish"))
+
+# run LIWC
+liwc_FARC <- liwcalike(FARC$text, spanish_dict)
+
+# neg and pos emotion
+FARC_neg <- data.frame(cbind(FARC_dates, as.numeric(liwc_FARC$EmoNeg)))
+FARC_neg$FARC_dates <- as.Date(FARC_dates, origin = "1970-01-01")
+FARC_pos <- data.frame(cbind(FARC_dates, as.numeric(liwc_FARC$EmoNeg)))
+FARC_pos$FARC_dates <- as.Date(FARC_dates, origin = "1970-01-01")
+
+
+####################################################################################
+# do the same for joint communiques
+joint <- read.csv("../MA-datasets/jointstatements.csv", stringsAsFactors = FALSE)
+# delete some empty documents
+joint <- filter(joint, text != "")
+joint <- slice(joint, -19)
+
+# get metadata: dates
+joint_meta <- select(joint, date)
+joint_dates <- as.Date(joint_meta[[1]], "%Y-%m-%d")
+
+# run LIWC
+liwc_joint <- liwcalike(joint$text, spanish_dict)
+
+# get neg and pos emotion
+joint_neg <- as.data.frame(cbind(joint_dates, as.numeric(liwc_joint$EmoNeg)))
+joint_neg$joint_dates <- as.Date(joint_neg$joint_dates, origin = "1970-01-01")
+joint_pos <- as.data.frame(cbind(joint_dates, as.numeric(liwc_joint$EmoPos)))
+joint_pos$joint_dates <- as.Date(joint_neg$joint_dates, origin = "1970-01-01")
+
+####################################################################################
+# get govt statements
+
+govt <- read.csv("govtstatements.csv", stringsAsFactors = FALSE)
+
+govt_meta <- select(govt, date)
+govt_dates <- as.Date(govt_meta[[1]], "%Y-%m-%d")
+
+# run LIWC
+liwc_govt <- liwcalike(govt$text, spanish_dict)
+
+govt_neg <- as.data.frame(cbind(govt_dates, as.numeric(liwc_govt$EmoNeg)))
+govt_neg$govt_dates <- as.Date(govt_neg$govt_dates, origin = "1970-01-01")
+govt_pos <- as.data.frame(cbind(govt_dates, as.numeric(liwc_govt$EmoPos)))
+govt_pos$govt_dates <- as.Date(govt_pos$govt_dates, origin = "1970-01-01")
+
+####################################################################################
+# let's graph negative emotion
+
+# plot(FARC_dates, FARC_neg$V2, xlim = c(as.Date("2011-01-01", "%Y-%m-%d"), as.Date("2016-06-01", "%Y-%m-%d")))
+
+# Neg emotion: base graph
+base_neg = ggplot() +
+  geom_line(data = FARC_neg, aes(x = FARC_dates, y = V2, color = "FARC statement")) +
+  geom_jitter() +
+#  geom_point(data = joint_neg, aes(x = joint_dates, y = V2, color = "Joint statement")) +
+  geom_line(data = joint_neg, aes(x = joint_dates, y = V2, color = "Joint statement")) +
+  geom_line(data = govt_neg, aes(x = govt_dates, y = V2, color = "Govt statement")) +
+  labs(
+    x = "Date",
+    y = "Percent Neg Emotion",
+    color = "Legend") +
+  scale_x_date(date_minor_breaks = "1 month",
+               limits = c(as.Date("2012-06-01", "%Y-%m-%d"), NA))
+
+# Neg emotion and major agreements/violence
+neg_major <- base_neg +
+  ggtitle("Major Events and Percent Negative Emotion Words in Statements") +
+  geom_vline(data = filter(dates, group == "major_agree"), mapping = aes(xintercept = as.numeric(date), color = "Major agreement"), linetype = 2) +
+  geom_vline(data = filter(dates, group == "major_viol"), mapping = aes(xintercept = as.numeric(date), color = "Major violence"), linetype = 1)
+
+
+# Neg emotion and ceasefire dates
+neg_cf <- base_neg +
+  ggtitle("Ceasefires and Percent Negative Emotion Words in Statements") +
+  geom_rect(aes(xmin=cf_start[1], xmax=cf_end[1], ymin=-Inf, ymax=Inf), fill = "yellow", linetype = 0, alpha = 0.3) + 
+  geom_rect(aes(xmin=cf_start[2], xmax=cf_end[2], ymin=-Inf, ymax=Inf), fill = "yellow", linetype = 0, alpha = 0.3) +
+  geom_rect(aes(xmin=cf_start[3], xmax=cf_end[3], ymin=-Inf, ymax=Inf), fill = "yellow", linetype = 0, alpha = 0.3) + 
+  geom_rect(aes(xmin=cf_start[4], xmax=cf_end[4], ymin=-Inf, ymax=Inf), fill = "yellow", linetype = 0, alpha = 0.3) + 
+  geom_rect(aes(xmin=cf_start[5], xmax=cf_end[5], ymin=-Inf, ymax=Inf), fill = "yellow", linetype = 0, alpha = 0.3)
+
+####################################################################################
+# Pos emotion: base graph
+base_pos = ggplot() +
+  geom_line(data = FARC_pos, aes(x = FARC_dates, y = V2, color = "FARC statement")) +
+  geom_jitter() +
+#  geom_point(data = joint_pos, aes(x = joint_dates, y = V2, color = "Joint statement")) +
+  geom_line(data = joint_pos, aes(x = joint_dates, y = V2, color = "Joint statement")) +
+  geom_line(data = govt_pos, aes(x = govt_dates, y = V2, color = "Govt statement")) +
+  labs(
+    x = "Date",
+    y = "Percent Positive Emotion",
+    color = "Legend") +
+  scale_x_date(date_minor_breaks = "1 month",
+               limits = c(as.Date("2012-06-01", "%Y-%m-%d"), NA))
+
+
+# pos emotion and major agreements
+pos_major <- base_pos +
+  ggtitle("Major Events and Percent Positive Emotion Words in Statements") +
+  geom_vline(data = filter(dates, group == "major_agree"), mapping = aes(xintercept = as.numeric(date), color = "Major agreement"), linetype = 2) +
+  geom_vline(data = filter(dates, group == "major_viol"), mapping = aes(xintercept = as.numeric(date), color = "Major violence"), linetype = 1)
+
+# pos emotion and ceasefires
+pos_cf <- base_pos +
+  ggtitle("Ceasefires and Percent Positive Emotion Words in Statements") +
+  geom_rect(aes(xmin=cf_start[1], xmax=cf_end[1], ymin=-Inf, ymax=Inf), fill = "yellow", linetype = 0, alpha = 0.3) + 
+  geom_rect(aes(xmin=cf_start[2], xmax=cf_end[2], ymin=-Inf, ymax=Inf), fill = "yellow", linetype = 0, alpha = 0.3) +
+  geom_rect(aes(xmin=cf_start[3], xmax=cf_end[3], ymin=-Inf, ymax=Inf), fill = "yellow", linetype = 0, alpha = 0.3) + 
+  geom_rect(aes(xmin=cf_start[4], xmax=cf_end[4], ymin=-Inf, ymax=Inf), fill = "yellow", linetype = 0, alpha = 0.3) + 
+  geom_rect(aes(xmin=cf_start[5], xmax=cf_end[5], ymin=-Inf, ymax=Inf), fill = "yellow", linetype = 0, alpha = 0.3)
+
+####################################################################################
+# let's graph 3rd person plural pronouns from FARC -- indicator of extremism
+FARC_ellos <- data.frame(ell = as.numeric(liwc_FARC$Ellos), date = FARC_dates)
+joint_ellos <- data.frame(ell = as.numeric(liwc_joint$Ellos), date = joint_dates)
+
+base_ellos = ggplot() +
+  geom_line(data = FARC_ellos, aes(x = date, y = ell, color = "FARC statement")) +
+  geom_jitter() +
+  # geom_point(data = joint_neg, aes(x = joint_dates, y = V2, color = "Joint statement")) +
+  geom_line(data = joint_ellos, aes(x = date, y = ell, color = "Joint statement")) +
+  # geom_line(data = govt_neg, aes(x = govt_dates, y = V2, color = "Govt statement")) +
+  labs(
+    x = "Date",
+    y = "Percent 3rd Person Pl Pronoun",
+    color = "Legend") +
+  scale_x_date(date_minor_breaks = "1 month",
+               limits = c(as.Date("2012-06-01", "%Y-%m-%d"), NA))
+
+# add major agreements and ceasefires
+ellos_major <- base_ellos +
+  ggtitle("Major Events and Use of 3rd Person Pl. Pronoun") +
+  geom_vline(data = filter(dates, group == "major_agree"), mapping = aes(xintercept = as.numeric(date), color = "Major agreement"), linetype = 2) +
+  geom_vline(data = filter(dates, group == "major_viol"), mapping = aes(xintercept = as.numeric(date), color = "Major violence"), linetype = 1)
+
+
+# run all the graphs
+base_neg
+base_pos
+base_ellos
+
+neg_cf
+neg_major
+pos_cf
+pos_major
+ellos_major
+
+###############################################
+# topic model
+trim_FARC <- quanteda::trim(FARC_dfm, minCount = 30, minDoc = 10)
+TM <- LDA(trim_FARC, 30, method = "Gibbs", control = list(burnin = 3, thin = 30, iter = 30, seed = 1234))
+top10words <- get_terms(TM, k = 10)
+doc_topics <- TM@gamma
+
+LDApost <- posterior(TM)
+
+jsonLDA <- createJSON(phi = LDApost$terms, 
+                      theta = LDApost$topics, 
+                      doc.length = ntoken(trim_FARC), 
+                      vocab = features(trim_FARC), 
+                      term.frequency = colSums(trim_FARC))
+serVis(jsonLDA, out.dir = "visCollLDA", open.browser = TRUE)
